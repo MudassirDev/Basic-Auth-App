5ad7601029ba8ef55390d59bef7b1fae636a33ac
MudassirDev <mughalmudassir966@gmail.com>
2024-03-02 12:14:30 +0500

    fixed env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID
+    apiKey: import.meta.env.VITE_REACT_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_REACT_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_REACT_FIREBASE_STORAGE,
+    messagingSenderId: import.meta.env.VITE_REACT_FIREBASE_SENDERID,
+    appId: import.meta.env.VITE_REACT_FIREBASE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
